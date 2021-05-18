04091a3f1994086be154e1aab50a9968eacd6633
lego man <rajawilliams24@gmail.com>
2021-05-17 17:10:27 -0700

    Improve formatting, add Checker Class
    Improved performance (by a lot) by (adding) checker class. This (should) make everything (a lot) easier (to) make from now on.

--- index.js
+++ index.js
@@ -1,19 +1,102 @@
-//THIS IS NOT A JOKE!!! USE LOL
-var none = require('none'); //this makes the program load faster!
-var throwErr = require('is-number'); // we are using var instead of const for speed!
-var isNum = require('throw-error')
-var True = require('false');
-var Oof = require('If'); //named oof so that it doesnt get confused with regular js if
-var Camelcase = require('camelcase');
-var sleep = require('system-sleep');
-var log = require("log");//just in case we need it later
-var chalk = require('chalk');
-var spaceBar = "-"
+const none = require('none')            ; //this makes the program load faster!
+var throwErr = require('is-number')		; // we are using var instead of const for speed!
+const isNum = require('throw-error')	;
+const True = require('false')			;
+var Oof = require('If')					; //named oof so that it doesnt get confused with regular js if
+var Camelcase = require('camelcase')	;
+var sleep = require('system-sleep')		;
+var log = require("log")				;//just in case we need it later
+const chalk = require('chalk')			;
+const spaceBar = "-"					;
+
+class Checker 							{
+	returnValue							: 
+	boolean
+										;
+	
+	constructor							(
+	value								:
+	boolean						
+										)
+										{
+	this								.
+	returnValue							=
+	value								}
+	
+	check								(
+	value								:
+	number								)
+										{
+		return this						.
+		returnValue						;	
+										}
+										}
 
 
-module.exports = function(num) {
-    console.log(chalk.bgHex("#ff00FF").bold(chalk.blue(Camelcase("[number-oddoreven]-Chalkulating-the-answer" + spaceBar + `${num.toString(num)}`))))
-        /* 5 seconds */sleep(1000);
-    Oof(!throwErr(num)).Then(()=>{isNum(new Error(Camelcase("Given-parameter-was-not-a-number!")))})
-return !True() ? !True() : True(); //here we(I) use a ternary operator to make sure that our result is always correct
-isNum(new Error(Camelcase("HOW DID WE GET HERE?? (Minecraft)")));}//we camel case, just in case. Get it? ;)
+module									.
+exports									= 
+function								(
+num										)
+										{
+    console								.
+	log									(
+	chalk
+										.
+	bgHex								(
+	"#ff00FF"
+										)
+										.
+	bold								(
+	chalk
+										.
+	blue								(
+	Camelcase							(
+	"[number-oddoreven]-Chalkulating-the-answer" 
+										+ 
+	spaceBar 							+ 
+	`${num.toString(num)}`
+										)
+										)
+										)
+/* 5 seconds in m(illiseconds) */sleep	(
+	1000								)
+										;
+    Oof									(
+										!
+	throwErr							(
+	num									)
+										)
+										.
+	Then								(
+										(
+										)
+										=>
+			  							{
+	isNum								(
+	new Error							(
+	Camelcase							(
+	"Given-parameter-was-not-a-number!"	)
+										)
+										)
+										}
+			 							)
+	return 
+										!
+	True								(
+										) 
+										?
+										
+	!True								(
+										)
+										:
+	True								(
+										)
+										; //here we(I) use a ternary operator to make sure that our result is always correct
+	isNum								(
+	new Error							(
+	Camelcase							(
+	"HOW DID WE GET HERE?? (Minecraft)"	) //we camel case, just in case. Get it? ;)
+										)
+										)
+										;
+										}
